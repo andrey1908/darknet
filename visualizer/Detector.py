16f580716b62098dfc4a08ce6662c778b22a939a
Keras <andrey.krishtopik@mail.ru>
2020-07-18 18:27:39 +0300

    free c image

--- visualizer/Detector.py
+++ visualizer/Detector.py
@@ -38,6 +38,7 @@ class Detector(QObject):
     def new_image(self, image_file):
         self.image_file = image_file
         self.image_pixmap = QPixmap(image_file)
+        self.lib.free_image(self.image_c)
         self.image_c = lib.load_image(image_file.encode(), 0, 0, lib.get_model_c(self.model))
         self.detect()
         self.draw()
@@ -88,3 +89,5 @@ class Detector(QObject):
     def __del__(self):
         if hasattr(self, 'model'):
             free_model(self.model)
+        if hasattr(self, 'image_c'):
+            self.lib.free_image(self.image_c)
